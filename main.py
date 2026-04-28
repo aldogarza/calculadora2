995e7a034f23d0922feae9ea9ae8133b5982b643
Aldo Garza <aldogarzaramirez@gmail.com>
2026-04-27 20:39:11 -0600

    Creando archivos necesarios main para el arranque del programa y una clase para el manejo de las funciones.

---
+++ main.py
@@ -0,0 +1,11 @@
+from Calculadora import Calculadora
+
+def menu():
+    print("\n--- CALCULADORA TDD ---")
+
+while True:
+    menu()
+    try:
+        pass
+    except Exception as e:
+        print("Error:", e)
